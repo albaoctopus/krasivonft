8af3dae952e632b9d6653c913ce521344d9b2091
Terekhov Anatoly <anatoly.terekhov@gmail.com>
2023-06-20 13:47:30 +0300

    zero2006

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,45 @@
+import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+/**
+ * Navigation bar that shows up on all pages.
+ * Rendered in _app.tsx file above the page content.
+ */
+export function Navbar() {
+  const address = useAddress();
+
+  return (
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <div className={styles.navSection}> {/* Center section */}
+          <Link href="/">
+            <Image
+              src="/l.webp"
+              width={200}
+              height={50}
+              alt="NFT marketplace sample logo"
+            />
+          </Link>
+        </div>
+        <div className={styles.navRight}> {/* Right section */}
+          <div className={styles.navConnect}>
+            <ConnectWallet theme="dark" btnTitle="Connect Wallet" />
+          </div>
+          {address && (
+            <Link className={styles.link} href={`/profile/${address}`}>
+              <Image
+                className={styles.profileImage}
+                src="/user-icon.png"
+                width={42}
+                height={42}
+                alt="Profile"
+              />
+            </Link>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
